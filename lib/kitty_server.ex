ffbc85e3c2cd1a6de37be5ca18fcf8952e57c61d
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2018-03-24 16:14:01 +0700

    Remove default lib and test implementations

--- lib/kitty_server.ex
+++ lib/kitty_server.ex
@@ -2,17 +2,4 @@ defmodule KittyServer do
   @moduledoc """
   Documentation for KittyServer.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> KittyServer.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/kitty_server_test.exs
+++ test/kitty_server_test.exs
@@ -1,8 +1,4 @@
 defmodule KittyServerTest do
   use ExUnit.Case
   doctest KittyServer
-
-  test "greets the world" do
-    assert KittyServer.hello() == :world
-  end
 end
