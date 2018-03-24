e9bc445a20ef788c564b3245ed77ea7b31bb0494
ivan <IvanIvanoff@users.noreply.github.com>
2018-03-24 16:13:05 +0700

    Initial commit

---
+++ lib/kitty_server.ex
@@ -0,0 +1,18 @@
+defmodule KittyServer do
+  @moduledoc """
+  Documentation for KittyServer.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> KittyServer.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
