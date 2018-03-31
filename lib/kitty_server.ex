8f866c54ddbb147c1853b3899c011de49914e314
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2018-03-31 17:46:55 +0700

    Add kitty_server structure

--- lib/kitty_server.ex
+++ lib/kitty_server.ex
@@ -2,4 +2,21 @@ defmodule KittyServer do
   @moduledoc """
   Documentation for KittyServer.
   """
+
+  def start_link() do
+  end
+
+  def loop(kitties) do
+  end
+
+  # Private functions
+
+  defp init() do
+  end
+
+  defp make_cat(name, color, description) do
+  end
+
+  defp terminate(kitties) do
+  end
 end
