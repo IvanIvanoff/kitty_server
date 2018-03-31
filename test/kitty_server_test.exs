1c4e45f3dc59379ce1b78e363f6d0c3376f23324
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2018-03-31 18:35:35 +0700

    Add testing of order_cat

--- test/kitty_server_test.exs
+++ test/kitty_server_test.exs
@@ -1,4 +1,14 @@
 defmodule KittyServerTest do
   use ExUnit.Case
-  doctest KittyServer
+
+  setup do
+    kitty_server = KittyServer.start_link()
+    {:ok, kitty_server: kitty_server}
+  end
+
+  test "kitty server serves kitties", %{kitty_server: kitty_server} do
+    KittyServer.order_cat(kitty_server, "Rumen", "Grey", "Writes poetry")
+
+    assert_receive %Kitty{color: "Grey", description: "Writes poetry", name: "Rumen"}, 5_000
+  end
 end
